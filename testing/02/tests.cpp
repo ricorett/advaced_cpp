ab10cd52218c48cd64d4875a0bdf6f14d660cfdb
Lana <115147586+ricorett@users.noreply.github.com>
2024-05-10 15:47:53 +0300

    delete comms

--- testing/02/tests.cpp
+++ testing/02/tests.cpp
@@ -17,12 +17,10 @@ TEST_CASE("List operations", "[list]") {
     }
 
     SECTION("PopBack removes element from the back of the list") {
-        // PopBack on empty list should do nothing
         l.PopBack();
         CHECK(l.Size() == 0);
         CHECK(!l.Empty());
 
-        // Add some elements first
         l.PushBack(3);
         l.PushBack(4);
         l.PushBack(5);
@@ -31,12 +29,10 @@ TEST_CASE("List operations", "[list]") {
     }
 
     SECTION("PopFront removes element from the front of the list") {
-        // PopFront on empty list should do nothing
         l.PopFront();
         CHECK(l.Size() == 0);
         CHECK(l.Empty());
 
-        // Add some elements first
         l.PushBack(6);
         l.PushBack(7);
         l.PushBack(8);
@@ -45,19 +41,16 @@ TEST_CASE("List operations", "[list]") {
     }
 
     SECTION("Complex scenario") {
-        // Add elements and perform operations
         l.PushBack(9);
         l.PushFront(10);
         l.PushBack(11);
         l.PushFront(12);
         CHECK(l.Size() == 4);
 
-        // Remove elements
         l.PopBack();
         l.PopFront();
         CHECK(l.Size() == 2);
 
-        // Add more elements
         l.PushFront(13);
         l.PushBack(14);
         CHECK(l.Size() == 4);
