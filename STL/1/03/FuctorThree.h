92230495c306460cbde8b7ed8da49956e45aa1f5
Lana <115147586+ricorett@users.noreply.github.com>
2024-05-17 12:33:56 +0300

    bold 03

---
+++ STL/1/03/FuctorThree.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "main.h"
+
+class FunctorThree {
+    FunctorThree(std::vector vec) : vec(c);
+};
---
+++ STL/1/03/main.cpp
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(){
+    FunctorThree func;
+    std::vector<int> data = {4, 1, 3, 6, 25, 54};
+
+    return 0;
+}
